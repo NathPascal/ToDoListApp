6c43199c04b27e76d3ab0e5c61a349da09dc38fc
NathPascal <nath.pascal03@gmail.com>
2025-01-13 16:28:59 +0100

    affichage listTasks

--- src/main/java/fr/fms/ToDoListApplication.java
+++ src/main/java/fr/fms/ToDoListApplication.java
@@ -21,13 +21,13 @@ public class ToDoListApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		LocalDateTime dateTime1 = LocalDateTime.of(2025, 3, 15, 10, 30);
-		LocalDateTime dateTime2 = LocalDateTime.of(2025, 4, 10, 14, 0);
-		LocalDateTime dateTime3 = LocalDateTime.of(2025, 5, 5, 18, 0);
-
-		taskRepository.save(new Task(null, "Rendez-vous médecin", dateTime1, "bilan annuel"));
-		taskRepository.save(new Task(null, "Réunion de travail", dateTime2, "Présentation du projet"));
-		taskRepository.save(new Task(null, "Anniversaire de Jean", dateTime3, "Acheter un cadeau"));
+//		LocalDateTime dateTime1 = LocalDateTime.of(2025, 3, 15, 10, 30);
+//		LocalDateTime dateTime2 = LocalDateTime.of(2025, 4, 10, 14, 0);
+//		LocalDateTime dateTime3 = LocalDateTime.of(2025, 5, 5, 18, 0);
+//
+//		taskRepository.save(new Task(null, "Rendez-vous médecin", dateTime1, "bilan annuel"));
+//		taskRepository.save(new Task(null, "Réunion de travail", dateTime2, "Présentation du projet"));
+//		taskRepository.save(new Task(null, "Anniversaire de Jean", dateTime3, "Acheter un cadeau"));
 	}
 }
 
--- src/main/java/fr/fms/web/TasksController.java
+++ src/main/java/fr/fms/web/TasksController.java
@@ -1,9 +1,13 @@
 package fr.fms.web;
 
 import fr.fms.dao.TaskRepository;
+import fr.fms.entities.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class TasksController {
@@ -12,7 +16,10 @@ public class TasksController {
     TaskRepository taskRepository;
 
     @GetMapping("/tasks")
-    public String tasks(){
+    public String tasks(Model model){
+        List<Task> tasks = taskRepository.findAll();
+        model.addAttribute("listTasks", tasks);
+
         return "tasks";
     }
 
