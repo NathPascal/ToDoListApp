9d6101e135d422ebbd51078e2e04d69c9dec3b15
NathPascal <nath.pascal03@gmail.com>
2025-01-13 15:41:14 +0100

    creation taskService findAll() et save()

---
+++ src/main/java/fr/fms/service/TaskService.java
@@ -0,0 +1,24 @@
+package fr.fms.service;
+
+import fr.fms.dao.TaskRepository;
+import fr.fms.entities.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void saveTask(Task task) {
+        taskRepository.save(task);
+    }
+    
+}
+
