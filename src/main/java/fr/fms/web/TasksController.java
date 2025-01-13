6c685e66a9eb10463bbe90db846be8f30808dc34
NathPascal <nath.pascal03@gmail.com>
2025-01-13 16:17:23 +0100

    creation controller

--- src/main/java/fr/fms/web/TasksController.java
+++ src/main/java/fr/fms/web/TasksController.java
@@ -1,9 +1,20 @@
 package fr.fms.web;
 
+import fr.fms.dao.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TasksController {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @GetMapping("/tasks")
+    public String tasks(){
+        return "tasks";
+    }
 
 
 
